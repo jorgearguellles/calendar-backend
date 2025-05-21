5299d5fecf2389bec466038661c20d67f77fc2ff
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2025-05-21 16:42:46 -0500

    ADD: Custom Middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,6 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 const createUser = (req, res = response) => {
   const { name, email, password } = req.body;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.status(201).json({
     ok: true,
@@ -22,14 +13,6 @@ const createUser = (req, res = response) => {
 
 const loginUser = (req, res = response) => {
   const { email, password } = req.body;
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
 
   res.status(200).json({
     ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -3,21 +3,26 @@
 */
 
 const express = require("express");
-const router = express.Router();
 const { check } = require("express-validator");
 const { createUser, loginUser, renewToken } = require("../controllers/auth");
+const { fieldValidators } = require("../middlewares/fieldValidators");
 
+const router = express.Router();
+
+// Middlewares
 const newUserValidator = [
   check("name", "Name is required").not().isEmpty(),
   check("email", "Email is required").isEmail(),
   check("password", "Password must be at least 6 characters long").isLength({
     min: 6,
   }),
+  fieldValidators,
 ];
 
 const loginUserValidator = [
   check("email", "Email is required").isEmail(),
   check("password", "Password is required").not().isEmpty(),
+  fieldValidators,
 ];
 
 // Routes
