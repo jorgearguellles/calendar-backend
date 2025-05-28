798e5f54282031fa19ba66e47f6e5952bb3a4584
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2025-05-28 17:15:26 -0500

    ADD: route to handle reload page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const { dbConnection } = require("./database/config");
 const cors = require("cors");
+const path = require("path");
 
 // create express app
 const app = express();
@@ -21,6 +22,10 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // start the server on port 3000
 app.listen(process.env.PORT, () => {
